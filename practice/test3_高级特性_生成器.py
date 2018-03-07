07e22c7b85b7b528f55a6b7fdb9d2afaac144573
Kun <pengzk@qwe.local>
2018-03-07 16:10:16 +0800

    update README.md

--- practice/test3_高级特性_生成器.py
+++ practice/test3_高级特性_生成器.py
@@ -5,7 +5,7 @@
 # 使用列表生成式将英文字母全部转换为小写，通过添加if语句保证列表生成式能正确地执行：
 L1 = ['Hello', 'World', 18, 'Apple', None]
 L2 = [s.lower() for s in L1 if isinstance(s, str)]
-print('lower([\'Hello\', \'World\', 18, \'Apple\', None]) =' , L2)
+print(r'lower([\'Hello\', \'World\', 18, \'Apple\', None]) =' , L2)
 
 
 # 高级特性->生成器
